db8a4613a6e28e232d3a30df4f4e8bd21862bb22
konrad1208 <32593523+konrad1208@users.noreply.github.com>
2022-06-29 17:00:33 +0100

    cohort table added

--- main_protocols5.py
+++ main_protocols5.py
@@ -19,7 +19,7 @@ def save_to_csv():
     df_app, df_add, df_rec = get_tables_1()
     df_json, df_scores, df_stren, df_weak, df_lang, df_stren_names, df_weak_names = get_tables_2()
     df_candidates, df_locations = get_tables_3()
-    df_academy = get_tables_4()
+    df_academy, df_cohort = get_tables_4()
     df_app.to_csv("tables2/applicant_table.csv")
     df_add.to_csv("tables2/address_table.csv")
     df_rec.to_csv("tables2/recruiter_table.csv")
@@ -33,10 +33,11 @@ def save_to_csv():
     df_weak.to_csv("tables2/weaknesses.csv")
     df_weak_names.to_csv("tables2/weak_names.csv")
     df_academy.to_csv("tables2/academy.csv")
+    df_cohort.to_csv("tables2/cohort.csv")
     # df_behaviours.to_pickle("tables/behaviours")
 
 
-save_to_csv()
+# save_to_csv()
 all_tables = []
 # print(pd.read_pickle("tables_pickle_file")[:20])
 
@@ -46,8 +47,8 @@ all_tables = []
 #     all_tables.append(i)
 # for i in get_tables_3():
 #     all_tables.append(i)
-# for i in get_tables_4():
-#     all_tables.append(i)
+for i in get_tables_4():
+    all_tables.append(i)
 
 # df_app, df_add, df_rec = get_tables_1()
 # df_json, df_scores, df_stren, df_weak = get_tables_2()
@@ -61,11 +62,11 @@ all_tables = []
 # save_excel(df_app, "applicants_table")
 # save_excel(df_add, "address_table")
 # save_excel(df_rec, "recruiters_table")
-# lim = 20
-# for i, v in enumerate(all_tables):
-#     if i >= lim:
-#         break
-#     print(v[:20], end="\n\n")
+lim = 20
+for i, v in enumerate(all_tables):
+    if i >= lim:
+        break
+    print(v[:20], end="\n\n")
 #
 # print(df_academy[:10])
 # print(df_behaviours[:10])
